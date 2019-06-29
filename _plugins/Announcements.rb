37a5d76b5f56402c79185e797e52e2471166d3c1
Cameron Gallarno <cgallarno@gmail.com>
2019-06-29 15:25:08 -0400

    Fixed newsletter sorting for newsletter page

--- _plugins/Announcements.rb
+++ _plugins/Announcements.rb
@@ -4,6 +4,10 @@ module Announcements
   		return bulletin.modified_time.strftime("%m/%d")
   	end
 
+  	def get_newsletter_date_from_posted_time(newsletter)
+  		return newsletter["posted_date"].strftime("%m/%d")
+  	end
+
   	def get_annoucment_message_from_bulletin(bulletin)
   		# <a href="bulletin/?date=6-17-2018">June 17th Bulletin</a>
   		date = Date.strptime(bulletin.basename, "%m %d %y")
@@ -27,16 +31,22 @@ module Announcements
   	end
 
   	def get_annoucment_message_from_newsletter(newsletter)
-  		return "<a href='newsletter/?v=" + newsletter.modified_time.strftime("%m%d%y") + "'>" + newsletter.basename + " is Available!</a>"
+  		return "<a href='newsletter/?v=" + newsletter["posted_date"].strftime("%m%d%y") + "'>" + newsletter["basename"] + " is Available!</a>"
   	end
 
     def generate(site)
       announcements = site.data["announcements"]
       bulletins = site.static_files.select { |f| f.data["bulletin"] == true }
-      newsletters = site.static_files.select { |f| f.data["newsletter"] == true }
+      newsletter_files = site.static_files.select { |f| f.data["newsletter"] == true }
+
+      newsletters = newsletter_files.map { |n|
+      	newsletter = JSON.parse(n.to_json)
+      	newsletter["posted_date"] = Date.strptime(newsletter["basename"], "%B %Y Newsletter")
+      	newsletter
+      }
 
       bulletins = bulletins.sort_by { | bulletin | Date.strptime(bulletin.basename, "%m %d %y") }
-      newsletters = newsletters.sort_by { | newsletter | Date.strptime(newsletter.basename, "%B %Y Newsletter") }
+      site.data["newsletters"] = newsletters.sort_by { | newsletter | newsletter["posted_date"] }
       # Add Latest Bulletin to Announcements
       announcements.push({
       	"date" => get_announcement_date_from_file_modified_time(bulletins.last),
@@ -44,8 +54,8 @@ module Announcements
       })
 
       announcements.push({
-      	"date" => get_announcement_date_from_file_modified_time(newsletters.last),
-      	"message" => get_annoucment_message_from_newsletter(newsletters.last)
+      	"date" => get_newsletter_date_from_posted_time(site.data["newsletters"].last),
+      	"message" => get_annoucment_message_from_newsletter(site.data["newsletters"].last)
       })
 
 
